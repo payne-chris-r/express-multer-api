65cf5dc58005df9d4439c27cd46cded8f33633bd
payne-chris-r <06cpayne@gmail.com>
2016-07-08 10:59:09 -0400

    Uses readFile to read file to be uploaded
    Not actually uploading anything yet, just setting the ground work.

---
+++ bin/s3-upload.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const fs = require('fs');
+
+let filename = process.argv[2] || '';
+
+fs.readFile(filename, (error, data)=>{
+  if(error){
+    return console.error(error);
+  }
+  console.log(`${filename} is ${data.length} bytes long`);
+});
