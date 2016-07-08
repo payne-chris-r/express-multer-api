cb0e4031e628195dda9a1107e9e3fc1f6e9c0a07
payne-chris-r <06cpayne@gmail.com>
2016-07-08 13:12:19 -0400

    Determine file/mime-type and start upload

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const fs = require('fs');
+const fileType = require('file-type');
 
 let filename = process.argv[2] || '';
+
+const mimeType = (data) => {
+  return Object.assign({
+    ext: 'bin',
+    mime: 'application/octet-stream'
+  }, fileType(data));
+};
 
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
@@ -15,6 +23,28 @@ const readFile = (filename) => {
   });
 };
 
+const awsUpload = function(file){
+  const options = {
+    ACL: "public-read",
+    Body: file.data,
+    Bucket: 'my.new.buckety',
+    ContentType: file.mine,
+    key: `test/test.${file.ext}`
+  };
+  return Promise.resolve(options);
+};
+
 readFile(filename)
-.then((data) => console.log(`${filename} is ${data.length} bytes long`))
+.then((data) => {
+  // let file = {};
+  let file = mimeType(data);
+  file.data = data;
+  return file;
+})
+.then(awsUpload)
+// .then((file) => {
+//
+// })
+// .then((data) => console.log(`${filename} is ${data.length} bytes long`))
+.then(console.log)
 .catch(console.error);
